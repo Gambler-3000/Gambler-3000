81d41d86df21d09811a3c77b3b42fcb59decc958
Gambler-3000 <lubieplackizkosza4136@gmail.com>
2025-09-23 18:24:38 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const playButton = document.getElementById('playButton');
+const numberResultSpan = document.getElementById('numberResult');
+const gameResultH2 = document.getElementById('gameResult');
+
+function playGame() {
+    const drawnNumber = Math.floor(Math.random() * 1000) + 1;
+
+    numberResultSpan.textContent = drawnNumber;
+
+    if (drawnNumber === 67) {
+        gameResultH2.textContent = 'Masiak wygrywa 5 piw! Gratulacje!';
+        gameResultH2.style.color = '#2ecc71';
+    } else {
+        gameResultH2.textContent = 'Masiak przejebał dom! Lepiej zagraj jeszcze raz!';
+        gameResultH2.style.color = '#e74c3c';
+    }
+}
+
+playButton.addEventListener('click', playGame);
